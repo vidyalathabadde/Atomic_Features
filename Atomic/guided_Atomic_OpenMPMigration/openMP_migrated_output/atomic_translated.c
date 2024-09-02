412e977518c7db0877a76bf1445c43b960c59a2f
vidyalathabadde <134362858+vidyalathabadde@users.noreply.github.com>
2024-09-02 19:00:36 +0530

    Update atomic_translated.c

--- Atomic/guided_Atomic_OpenMPMigration/openMP_migrated_output/atomic_translated.c
+++ Atomic/guided_Atomic_OpenMPMigration/openMP_migrated_output/atomic_translated.c
@@ -13,8 +13,6 @@ int main(){
     double captureSum = 0.0;
     double updateSum = 0.0;
 
-    // the atomic construct prevents reading the value while a gang/worker/vector is writing and vice versa
-    // this is the read clause read the value of one variable into another variable
 #pragma omp target teams loop map(tofrom:data[0:n]) map(from:readSum)
     for(int x = 0; x < n; ++x){
         if(data[x] >= n/2){
@@ -23,8 +21,6 @@ int main(){
         }
     }
 
-    // the atomic construct prevents reading the value while a gang/worker/vector is writing and vice versa
-    // this is the write clause that only allows a direct write to a variable from a expression
 #pragma omp target teams loop map(tofrom:data[0:n]) map(from:writeSum)
     for(int x = 0; x < n; ++x){
         if(data[x] >= n/2){
@@ -33,7 +29,6 @@ int main(){
         }
     }
 
-    //this is the capture clause that the update to the expression into another variable
 #pragma omp target teams loop map(tofrom:data[0:n]) map(from:captureSum)
     for(int x = 0; x < n; ++x){
         if(data[x] >= n/2){
@@ -44,7 +39,6 @@ int main(){
 
     std::cout << captureSum << std::endl;
 
-    //this is the update clause which locks the update of a particualar variable from certain operations
 #pragma omp target teams loop map(tofrom:data[0:n]) map(from:updateSum)
     for(int x = 0; x < n; ++x){
         if(data[x] >= n/2){
